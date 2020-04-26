e6fa850de0f03ee5792af4faa197f04ff4ffd9cc
Bianca <biancarbartolomei@gmail.com>
2020-04-26 09:13:08 -0300

    [ADD]Envio e armazenamento de mensagens

--- client.py
+++ client.py
@@ -12,9 +12,11 @@ except socket.error as e:
 
 Response = ClientSocket.recv(1024)
 while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
     Response = ClientSocket.recv(1024)
     print(Response.decode('utf-8'))
+    Input = input('Say Something: ')
+    ClientSocket.send(str.encode(Input))
+    # Response = ClientSocket.recv(1024)
+    # print(Response.decode('utf-8'))
 
 ClientSocket.close()
